8a922a2c12ef350f412dcbf784477c0bab845cc6
Robert Todar <robertptodar@gmail.com>
2019-08-24 15:16:37 -0700

    Navbar design complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import logo from "../resources/favicon.ico";
+import M from "materialize-css";
 
 function Navbar() {
+  const [links] = useState([
+    {
+      title: "Girls Night",
+      path: "#!"
+    },
+    {
+      title: "Guys Night",
+      path: "#!"
+    },
+    {
+      title: "Worship Songs",
+      path: "#!"
+    }
+  ]);
+
+  const paths = links.map(({ title, path }) => (
+    <li key={title}>
+      <a href={path} alt={title}>
+        {title}
+      </a>
+    </li>
+  ));
+
+  useEffect(() => {
+    M.Sidenav.init(document.querySelectorAll(".sidenav"), {});
+    return () => {};
+  }, []);
+
   return (
-    <nav className="grey darken-3">
-      <div class="container">
-        <h5
-          style={{
-            display: "inline",
-            border: ".1rem solid #fff",
-            padding: ".4rem"
-          }}
-          href="#"
-          class=""
-        >
-          FH
-        </h5>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li>
-            <a href="sass.html">Sass</a>
-          </li>
-          <li>
-            <a href="badges.html">Components</a>
-          </li>
-          <li>
-            <a href="collapsible.html">JavaScript</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <React.Fragment>
+      <nav className="grey darken-4">
+        <div className="container">
+          <a href="#!" className="brand-logo">
+            <img style={{ marginTop: ".4rem" }} src={logo} alt="Logo" />
+          </a>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            {paths}
+          </ul>
+        </div>
+      </nav>
+      <ul className="sidenav" id="mobile-demo">
+        {paths}
+      </ul>
+    </React.Fragment>
   );
 }
 
